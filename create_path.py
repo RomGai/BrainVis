7a6fa9a579f6f32c991cc96fd8741e8b5b760f9d
RomGai <114910083+RomGai@users.noreply.github.com>
2023-12-16 21:16:34 +0800

    Add files via upload

---
+++ create_path.py
@@ -0,0 +1,28 @@
+import os
+
+folder1 = "picture-gene"
+folder2 = "picture-gene-onlygt"
+folder3 = "exp/epilepsy/test"
+folder4 = "data/EEG"
+folder5 = "data/EEG_divided"
+folder6 = "data/EEG_Feature_Label"
+folder7 = "data/image"
+folder8 = "data/image_text"
+folder9 = "data/image_text_CLIP"
+folder10 = "data/imagelabel_text_CLIP"
+folder11 = "pretrained_model"
+
+os.makedirs(folder1)
+os.makedirs(folder2)
+os.makedirs(folder3)
+os.makedirs(folder4)
+os.makedirs(folder5)
+os.makedirs(folder6)
+os.makedirs(folder7)
+os.makedirs(folder8)
+os.makedirs(folder9)
+os.makedirs(folder10)
+os.makedirs(folder11)
+
+
+
